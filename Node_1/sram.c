a33f0f5a87b1553d67ecd9b5717ddd5890df885a
margot-frances <mgfrance@stud.ntnu.no>
2024-09-10 19:54:57 +0200

    Update sram.c

--- Node_1/sram.c
+++ Node_1/sram.c
@@ -10,7 +10,7 @@
 
 void sram_init(){
     MCUCR |= (1 << SRE);
-    SFIOR |= (1 << XMM2); //check that too, disabled I/O pins for the sram
+    SFIOR |= (3 << XMM0); //check that too, disabled I/O pins for the sram
 }
 
 
