4be48036d3d50924f2d56a6bc1a539b201e0b261
Alix <128033856+Ali15130@users.noreply.github.com>
2024-09-12 12:41:46 +0200

    Update uart.h

--- Node_1/uart.h
+++ Node_1/uart.h
@@ -1,24 +1,10 @@
-#ifndef USART_DRIVER_H
-#define USART_DRIVER_H
+#ifndef UART_H
+#define UART_H
 
-#include <avr/io.h>
-#include <stdio.h>
-#include <avr/interrupt.h>
-#include <util/delay.h>
-#include <stdint.h>
+void USART_Init(unsigned int ubrr); // Function to initialize UART communication
+void USART_Transmit(unsigned char data); // Function to transmit a character over UART
+unsigned char USART_Receive(void); // Function to receive a character via UART
+int uart_putchar(char c, FILE *stream); // Function to send a character through UART for printf support
+void init_printf(void); // Function to initialize printf to use UART
 
-// Constants and Macros
-#define F_CPU 4.9152E8 // CPU Clock Speed
-#define BAUD 9600       // Baud rate for UART communication
-#define MYUBRR F_CPU/16/BAUD-1 // Calculating UBRR value based on baud rate and clock speed
-
-// Function prototypes
-void USART_Init(unsigned int ubrr);               // Function to initialize USART
-void USART_Transmit(unsigned char data);          // Function to transmit a single character over USART
-unsigned char USART_Receive(void);                // Function to receive a single character from USART
-void USART_Notif_receive_data(void);              // Function to enable notification when data is received
-//ISR(USART0_RXC_vect);                             // Interrupt Service Routine for received data
-void link_printf(void);                           // Function to link printf to USART
-
-#endif // USART_DRIVER_H
-
+#endif /* UART_H */
