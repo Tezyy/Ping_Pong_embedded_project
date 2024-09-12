80c3f3e8b1083e071ebb631c26e122cc8c363f15
Alix <128033856+Ali15130@users.noreply.github.com>
2024-09-12 12:34:47 +0200

    Update uart.c

--- Node_1/uart.c
+++ Node_1/uart.c
@@ -1,67 +1,61 @@
-/* <INFORMATION>    
-This code contains sample code parts from ATmega162 Datasheet*/
+#define F_CPU 4915200UL
+#include <util/delay.h>
 
 #include <avr/io.h>
 #include <stdio.h>
 #include <avr/interrupt.h>
-#define F_CPU 4.9152E8 //Clock Speed
-#include <util/delay.h>
 #include  <stdint.h>
+
 #include "uart.h"
 
-#define BAUD 9600 //Baud USART
-#define MYUBRR F_CPU/16/BAUD-1 
+#define FOSC 4915200UL
+#define BAUD 9600
+#define MYUBRR FOSC/16/BAUD-1
 
 
-void USART_Init( unsigned int ubrr ) // Initialize function of register USART
+void USART_Init( unsigned int ubrr )
 {
-/* Set baud rate */
-UBRR0H = (unsigned char)(ubrr>>8);
-UBRR0L = (unsigned char)ubrr;
-/* Enable receiver and transmitter */
-UCSR0B = (1<<RXEN0)|(1<<TXEN0);
-/* Set frame format: 8data, 2stop bit */ 
-UCSR0C = (1<<URSEL0)|(1<<USBS0)|(3<<UCSZ00); // If more speed desired we can use 1stop bit : UCSRC = (1 << URSEL0) | (1 << UCSZ00) | (1 << UCSZ01);
+      /* Set baud rate */
+      UBRR0H = (unsigned char)(ubrr>>8);
+      UBRR0L = (unsigned char)ubrr;
+      /* Enable receiver and transmitter */
+      UCSR0B = (1<<RXEN0)|(1<<TXEN0);
+      /* Set frame format: 8data, 2stop bit */
+      UCSR0C = (1<<URSEL0)|(1<<USBS0)|(3<<UCSZ00);
 }
 
 
-void USART_Transmit( unsigned char data ) //USART transmit data function 
+void USART_Transmit( unsigned char data )
 {
-/* Wait for empty transmit buffer */
-while ( !( UCSR0A & (1<<UDRE0)) );
-/* Put data into buffer, sends the data */
-UDR0 = data;
+      /* Wait for empty transmit buffer */
+      while ( !( UCSR0A & (1<<UDRE0)) )
+      ;
+      /* Put data into buffer, sends the data */
+      UDR0 = data;
 }
 
 
-unsigned char USART_Receive( void ) //USART receiving data function
+unsigned char USART_Receive( void )
 {
-/* Wait for data to be received */
-while ( !(UCSR0A & (1<<RXC0)) );
-/* Get and return received data from buffer */
-return UDR0;
+      /* Wait for data to be received */
+      while ( !(UCSR0A & (1<<RXC0)) )
+      ;
+      /* Get and return received data from buffer */
+      return UDR0;
 }
 
-/*Use of global interrupt to notify when a new character is received*/
-void USART_Notif_receive_data (void)
-{ 
-/*use of UCSRB0 but UCSRB1 exists ?? --> UCSRB is a register for USART control, and there's typically
-only one UCSRB for each USART peripheral(unless there are multiple USART peripherals, like USART0 and USART1).
-On an ATmega microcontroller with two USARTs, we have UCSR0B for USART0 and UCSR1B for USART1.
-We can configure the second UART with the equivalent UCSR1B register.*/
-    UCSR0B |= (1<<RXCIE0); //To enable interrupt-based reception we need to set HIGH the Receive Complete Interrupt Enable(RXCIE) bit in UCSRB
-    sei();
-}
 
-/*When the RXC flag is set (when a byte is received), this ISR is executed.*/
-ISR(USART0_RXC_vect)//Vector name on https://onlinedocs.microchip.com/pr/GUID-317042D4-BCCE-4065-BB05-AC4312DBC2C4-en-US-2/index.html?GUID-F889605B-692F-493A-8BE7-F0FBACF1715B
-{ 
-    unsigned char received_data = UDR0; //take the char which is in UDR register
-    /* Possibility to make a "circular buffer" to store multiple data */
-}
-
-/*Link printf function */
-void link_printf(void)
+int uart_putchar(char c, FILE *stream)
 {
-    fdevopen(USART_Transmit,USART_Receive);
+      if (c == '\n') {
+            USART_Transmit('\r');  // Send carriage return before newline
+      }
+      USART_Transmit(c);  // Transmit the character
+      return 0;
+}
+
+void init_printf(void)
+{
+      static FILE uart_stdout = FDEV_SETUP_STREAM(uart_putchar, NULL, _FDEV_SETUP_WRITE);
+      stdout = &uart_stdout;  // Set stdout to use uart_putchar
 }
