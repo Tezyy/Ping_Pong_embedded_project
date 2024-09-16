0877b06e48d9df6d6d3ceae588182f1b3bd84b0b
Alix <128033856+Ali15130@users.noreply.github.com>
2024-09-16 18:34:51 +0200

    Update sram.c

--- Node_1/sram.c
+++ Node_1/sram.c
@@ -1,62 +1,67 @@
-#include <stdlib.h>
+#define F_CPU 4915200UL
+#include <util/delay.h>
+
 #include <avr/io.h>
+#include <avr/sleep.h>
 #include <stdio.h>
 #include <stdlib.h>
 
-#include "sram.h"
-#include "bit_macros.h"
-
 #define BASE_ADDRESS_SRAM 0x1800
-#define BASE_ADDRESS 0x1000
-#define LED_PORT 0x0800
+
+#include "sram.h"
+
 
 void XMEM_init ( void )
 {
-	set_bit(MCUCR,SRE);
-	set_bit(SFIOR,XMM0); //Remove 4 Most Significant Bits from address so that JTAG interface doesn't crash
-	//MCUCR |= (1 << SRE ); // enable XMEM
-	//SFIOR |= (1 << XMM0); // Mask PC7-PC4 for JTAG (XMM2 ?)
+	MCUCR |= (1 << SRE); // enable XMEM
+	SFIOR |= (1 << XMM2); // Mask PC7-PC4 for JTAG
+
 }
 
 
 void XMEM_write(uint8_t data, uint16_t addr)
 {
-    volatile char *ext_mem = (char *)BASE_ADDRESS;
-    ext_mem[addr] = data;
+	volatile char *ext_mem = (char *)BASE_ADDRESS_SRAM;
+	ext_mem[addr] = data;
 }
 
+uint8_t XMEM_read(uint16_t addr){
+	volatile char *ext_mem = (char *) BASE_ADDRESS_SRAM;
+	uint8_t ret_val = ext_mem[addr]; //Attention ici à comprendre...
+	return ret_val; //Idem...
+}
 
 void SRAM_test(void)
-    {
+{
 	volatile char *ext_ram = (char *) 0x1800; // Start address for the SRAM
-        uint16_t ext_ram_size = 0x800;
-        uint16_t write_errors = 0;
-        uint16_t retrieval_errors = 0;
-        printf("Starting SRAM test...\n");
-        // rand() stores some internal state, so calling this function in a loop will
-        // yield different seeds each time (unless srand() is called before this function)
-        uint16_t seed = rand();
-        // Write phase: Immediately check that the correct value was stored
-        srand(seed);
-        for (uint16_t i = 0; i < ext_ram_size; i++) {
-            uint8_t some_value = rand();
-            ext_ram[i] = some_value;
-            uint8_t retreived_value = ext_ram[i];
-            if (retreived_value != some_value) {
-                printf("Write phase error: ext_ram[%4d] = %02X (should be %02X)\n", i, retreived_value, some_value);
-                write_errors++;
-            }
-        }
-        // Retrieval phase: Check that no values were changed during or after the write phase
-        srand(seed);
-        // reset the PRNG to the state it had before the write phase
-        for (uint16_t i = 0; i < ext_ram_size; i++) {
-            uint8_t some_value = rand();
-            uint8_t retreived_value = ext_ram[i];
-            if (retreived_value != some_value) {
-                printf("Retrieval phase error: ext_ram[%4d] = %02X (should be %02X)\n", i, retreived_value, some_value);
-                retrieval_errors++;
-            }
-        }
-        printf("SRAM test completed with \n%4d errors in write phase and \n%4d errors in retrieval phase\n\n", write_errors, retrieval_errors);
+	uint16_t ext_ram_size = 0x800;
+	uint16_t write_errors = 0;
+	uint16_t retrieval_errors = 0;
+	printf("Starting SRAM test...\n");
+	// rand() stores some internal state, so calling this function in a loop will
+	// yield different seeds each time (unless srand() is called before this function)
+	uint16_t seed = rand();
+	// Write phase: Immediately check that the correct value was stored
+	srand(seed);
+	for (uint16_t i = 0; i < ext_ram_size; i++) {
+		uint8_t some_value = rand();
+		ext_ram[i] = some_value;
+		uint8_t retreived_value = ext_ram[i];
+		if (retreived_value != some_value) {
+			printf("Write phase error: ext_ram[%4d] = %02X (should be %02X)\n", i, retreived_value, some_value);
+			write_errors++;
+		}
+	}
+	// Retrieval phase: Check that no values were changed during or after the write phase
+	srand(seed);
+	// reset the PRNG to the state it had before the write phase
+	for (uint16_t i = 0; i < ext_ram_size; i++) {
+		uint8_t some_value = rand();
+		uint8_t retreived_value = ext_ram[i];
+		if (retreived_value != some_value) {
+			printf("Retrieval phase error: ext_ram[%4d] = %02X (should be %02X)\n", i, retreived_value, some_value);
+			retrieval_errors++;
+		}
+	}
+	printf("SRAM test completed with \n%4d errors in write phase and \n%4d errors in retrieval phase\n\n", write_errors, retrieval_errors);
 }
