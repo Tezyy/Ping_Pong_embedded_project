efde3829a9ec40d7023fabc8cb7a6cd82ee4bc27
Alix <128033856+Ali15130@users.noreply.github.com>
2024-09-18 15:35:49 +0200

    Update main.c

--- Node_1/main.c
+++ Node_1/main.c
@@ -23,6 +23,18 @@ void exercise1 (void){
 }
 
 void exercise2 (void){
+	//Pour faire ce test we have to comment XMEM_init parce que en gros dans XMEM _init SRE overide toutes les directions de pin
+	//donc set les DDR manuellement c'est contradictoire avec set SRE, et le reste du temps SRE gère si les pins seront en entrée
+	//ou en sortie automatiquement 
+	
+	//SRAM start:         0x1800    1100 000000000
+	//SRAM end:           0x1FFF    1111 111111111
+	//ADC start:          0x1400    1010 000000000
+	//ADC end:            0x17FF    1011 111111111
+	//OLED-data start:    0x1200    1001 000000000
+	//OLED-data end:      0x13FF    1001 111111111
+	//OLED-command start: 0x1000    1000 000000000
+	//OLED-command end:   0x11FF    1000 111111111
 	
 	//PORTA = 0b00000010; //Sending an adress.
 	PORTC = 0b00001000; //Sending an SRAM adress
@@ -48,38 +60,19 @@ void exercise2 (void){
 	PORTE = 0b00; //Sets ALE low. Now the address value is stored.
 	_delay_ms(1000);
 }
-void exercise2_bis (void){
-	//SRAM start:         0x1800    1100 000000000
-	//SRAM end:           0x1FFF    1111 111111111
-	//ADC start:          0x1400    1010 000000000
-	//ADC end:            0x17FF    1011 111111111
-	//OLED-data start:    0x1200    1001 000000000
-	//OLED-data end:      0x13FF    1001 111111111
-	//OLED-command start: 0x1000    1000 000000000
-	//OLED-command end:   0x11FF    1000 111111111
-	
-	XMEM_read(0x1100);
-	//XMEM_write(0x11111111,0x1801);
-	_delay_ms(1000);
-
-}
 
 int main(void)
 {
-	DDRE = 0b10; //Sets ALE (pin 1 on port E) as output.
-	DDRA = 0xFF; //Because we read from PORTA we need to set it as an output.
-	DDRC = 0b111; //Three first C pins as outputs
 	
 	USART_Init(MYUBRR);
 	init_printf();
-	//XMEM_init();
+	XMEM_init();
 	//SRAM_test();
 	
 	while(1)
     {
 		//exercise1();
-		exercise2();
-		//exercise2_bis();
+		//exercise2();
     }
 	return(0);
 }
