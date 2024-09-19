73c6bef584c2ade37965f954226856769c1e02d2
Alix <128033856+Ali15130@users.noreply.github.com>
2024-09-19 16:22:04 +0200

    Update sram.h

--- Node_1/sram.h
+++ Node_1/sram.h
@@ -11,11 +11,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// Base addresses for external devices
-#define BASE_ADDRESS_SRAM 0x1800
-#define BASE_ADDRESS_OLED 0x1000
-#define BASE_ADDRESS_ADC 0x1400
-
 // Function prototypes
 void XMEM_init(void);
 void XMEM_write(uint8_t data, uint16_t addr);
