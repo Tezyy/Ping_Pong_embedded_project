e9d9ddbe895654c73418cd2669b959de4949a44f
margot-frances <mgfrance@stud.ntnu.no>
2024-09-24 20:13:02 +0200

    Update adc.c

--- Node_1/adc.c
+++ Node_1/adc.c
@@ -6,11 +6,34 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#include "sram.h"
+#include "adc.h"
 
+void adc_init(){
+	DDRD |= (1 << DDD5); //set PD5 to output
+	
+	// fast PWM mode
+	TCCR1A |= (1<<WGM10) | (1<<WGM11);
+	TCCR1B |= (1<<WGM12) | (1<<WGM13);
+	
+	//toggle
+	TCCR1A &= ~(1<<COM1A1);
+	TCCR1A |= (1<<COM1A0);
+	
+	//clock select bit
+	TCCR1B &= ~(1<<CS12);
+	TCCR1B &= ~(1<<CS11);
+	TCCR1B |= (1<<CS10);
+	
+	//Had to be zero
+	TCCR1A &= ~(1<<FOC1A);
+	
+	OCR1AH=0x0;
+	OCR1AL=0x1;
+	
+	DDRD &= ~(1<<DDD4); //set PD4 to input
+	 
+}
 
-//fonctions à coder selon lab lecture 2
-adc_init (void);
 uint8_t adc_read(uint8_t channel); //volatile
 pos_calibrate();
 pos_t pos_read(void);
