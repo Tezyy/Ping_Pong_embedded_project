a106f89cc3009b4e8c5649b83a1e2e7e45ebb891
margot-frances <mgfrance@stud.ntnu.no>
2024-09-27 17:21:07 +0200

    Update adc.h

--- Node_1/adc.h
+++ Node_1/adc.h
@@ -6,7 +6,14 @@
 
 #define F_CPU 4915200UL
 
+typedef struct {
+	uint8_t joystick_x;
+	uint8_t joystick_y;
+	uint8_t slider_left;
+	uint8_t slider_right;
+} adc_data_t;
+
 void adc_init(void);
-uint8_t adc_read(uint8_t channel);
+adc_data_t adc_read();
 
 #endif // ADC_H
