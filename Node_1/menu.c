09f405ae7d9333cc05b63836ead8bc2a5a241dbb
margot-frances <mgfrance@stud.ntnu.no>
2024-10-03 11:14:36 +0200

    Update menu.c

--- Node_1/menu.c
+++ Node_1/menu.c
@@ -1,1 +1,34 @@
 #include "menu.h"
+#include "joystick.h"
+#include <string.h>
+
+
+void fleche(){
+	oled_print_char('<');oled_print_char('-');
+}
+
+const char* menu_options[NUM_OPTIONS] = {
+	"Option 1",
+	"Option 2"
+};
+
+void print_menu() {
+	oled_clear();
+	for (uint8_t i=0; i<NUM_OPTIONS; i++){
+		oled_set_pos(i,0);
+		oled_print_string(menu_options[i]);
+	}
+}
+
+void choix_menu(uint8_t current_selection){
+	for (uint8_t i=0; i<NUM_OPTIONS; i++){
+		if (i== current_selection){
+			oled_set_pos(i,110);
+			fleche();
+		}
+		else {
+			oled_set_pos(i,110);
+			oled_print_string("  ");
+		}
+	}
+}
