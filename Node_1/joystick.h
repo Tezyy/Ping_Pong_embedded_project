d014d87b387cc5237bb253af3ec7dfd19c2fb569
margot-frances <mgfrance@stud.ntnu.no>
2024-10-03 11:10:14 +0200

    Update joystick.h

--- Node_1/joystick.h
+++ Node_1/joystick.h
@@ -3,11 +3,6 @@
 
 #include <stdint.h> 
 
-typedef struct {
-	uint8_t x_percent; 
-	uint8_t y_percent; 
-} JoystickPosition;
-
 typedef enum {
 	LEFT,
 	RIGHT,
@@ -16,13 +11,24 @@ typedef enum {
 	NEUTRAL
 } JoystickDirection;
 
-#define ADC_MIN 0
-#define ADC_MAX 255
-#define JOYSTICK_CENTER 128 
+typedef struct {
+	int16_t x_center; 
+	int16_t y_center;  
+} JoystickCalibration;
 
-#define THRESHOLD 30
+typedef struct {
+	int16_t x_percent; 
+	int16_t y_percent;
+} JoystickPosition;
 
-JoystickPosition getJoystickPosition(uint8_t adc_x, uint8_t adc_y);
+typedef struct {
+	int16_t left_percent;  // Left slider position in percentage (-100 to 100 or 0 to 100)
+	int16_t right_percent; // Right slider position in percentage (-100 to 100 or 0 to 100)
+} SliderPosition;
+
+JoystickCalibration calibrateJoystick();
+
+JoystickPosition getJoystickPosition(uint8_t adc_x, uint8_t adc_y, JoystickCalibration calib);
 
 JoystickDirection getJoystickDirection(JoystickPosition pos);
 
