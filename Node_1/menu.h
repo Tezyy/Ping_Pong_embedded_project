3042738c1c432b2112b63c726ce8371297beb30d
margot-frances <mgfrance@stud.ntnu.no>
2024-10-03 11:15:04 +0200

    Update menu.h

--- Node_1/menu.h
+++ Node_1/menu.h
@@ -1,1 +1,21 @@
+#ifndef GRAPHICS_H
+#define GRAPHICS_H
+#include "joystick.h"
+#include "oled.h"
+
+#include <avr/io.h>
+#include <avr/sleep.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <avr/pgmspace.h>
+#include <string.h>
+
+#define NUM_OPTIONS 2
+
+void fleche();
+void print_menu();
+void choix_menu(uint8_t current_selection);
+
+#endif // GRAPHICS_H
 
