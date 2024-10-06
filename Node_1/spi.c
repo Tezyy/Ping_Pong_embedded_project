ace43a44a6b63740d2102c5344d111bbf931ac9d
margot-frances <mgfrance@stud.ntnu.no>
2024-10-06 15:51:09 +0200

    Update spi.c

--- Node_1/spi.c
+++ Node_1/spi.c
@@ -1,1 +1,10 @@
 #include spi.h
+
+void init_spi(){
+  DDRB |= (1<<PB5) | (1<<PB7) | (1<<PB4); //MOSI(PB5) SCK(PB7) SS(PB4) output
+  DDRB &= ~(1 << PB6); // MISO (PB6) input
+  SPCR = (1 << SPE) | (1 << MSTR) | (1 << SPR0); // enable SPI, set SPI mode 0, set SCK rate to F_CPU/ 16 
+  PORTB |= (1 << PB4); // active SS
+}
+
+
