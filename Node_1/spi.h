4d7c39adccc09e6342e9061c0446dc08e3a82319
margot-frances <mgfrance@stud.ntnu.no>
2024-10-06 16:39:40 +0200

    Update spi.h

--- Node_1/spi.h
+++ Node_1/spi.h
@@ -1,1 +1,14 @@
+#ifndef SPI_H
+#define SPI_H
+
+#include <avr/io.h>
+#include <stdint.h>
+
+void init_spi(void);
+void spi_transmit(uint8_t data);
+uint8_t spi_receive(void);
+void spi_select_slave(void);
+void spi_deselect_slave(void);
+
+#endif // SPI_H
 
