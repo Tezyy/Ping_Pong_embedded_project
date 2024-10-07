8db96e47c8912563383f26b38ce224e2bdc2d34b
margot-frances <mgfrance@stud.ntnu.no>
2024-10-07 19:18:05 +0200

    Update MCP2515_driver.c

--- Node_1/MCP2515_driver.c
+++ Node_1/MCP2515_driver.c
@@ -1,59 +1,78 @@
 #include "MCP2515.h"
+#include "spi.h"
+#include "bit_macros.h"
+
 
 #include <avr/io.h>
 #include <avr/sleep.h>
-#include "spi.h"
-uint8_t read(uint8_t adress){
-    clear_bit(PORTB, PB4);
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
 
-    spi_receive(MCP_READ);
-    spi_receive(adress);
-    uint8_t result = spi_receive();
 
-    set_bit(PORTB, PB4);
+uint8_t mcp2515_read(uint8_t adress){
+	clear_bit(PORTB, PB4);
 
-    return result;
+	spi_transmit(MCP_READ);
+	spi_transmit(adress);
+	uint8_t result = spi_receive();
 
-};
+	set_bit(PORTB, PB4);
 
-void write(uint8_t adress, uint8_t data ){
-    clear_bit(PORTB, PB4);
+	return result;
 
-    spi_receive(MCP_WRITE);
-    spi_receive(adress);
-    spi_receive(data);
+}
 
-    set_bit(PORTB, PB4);
-};
+uint8_t mcp2515_write(uint8_t adress, uint8_t data ){
+	clear_bit(PORTB, PB4);
+
+	spi_transmit(MCP_WRITE);
+	spi_transmit(adress);
+	spi_transmit(data);
+
+	set_bit(PORTB, PB4);
+}
 
 void req_to_send(uint8_t stat){
-    clear_bit(PORTB, PB4);
-    spi_receive(stat);
-    set_bit(PORTB, PB4);
-};
+	clear_bit(PORTB, PB4);
+	spi_transmit(stat);
+	set_bit(PORTB, PB4);
+}
 
 uint8_t mcp2515_read_status(){
-    clear_bit(PORTB, PB4);
-    uint8_t result = spi_receive(MCP_READ_STATUS);
-    set_bit(PORTB, PB4);
-};
+	clear_bit(PORTB, PB4);
+	spi_transmit(MCP_READ_STATUS);
+	uint8_t result = spi_receive();
+	set_bit(PORTB, PB4);
+	return result;
+}
 
 void mcp2515_modify_bit(uint8_t address, uint8_t mask, uint8_t data){
-    clear_bit(PORTB, PB4);
-    spi_receive(MCP_BITMOD);
-    spi_receive(address);
-    spi_receive(mask);
-    spi_receive(data);
-    set_bit(PORTB, PB4);
-};
+	clear_bit(PORTB, PB4);
+	spi_transmit(MCP_BITMOD);
+	spi_transmit(address);
+	spi_transmit(mask);
+	spi_transmit(data);
+	set_bit(PORTB, PB4);
+}
 
 void mcp2515_reset(){
-    clear_bit(PORTB, PB4);
-    spi_receive(MCP_RESET);
-    set_bit(PORTB, PB4);
-};
+	clear_bit(PORTB, PB4);
+	spi_transmit(MCP_RESET);
+	set_bit(PORTB, PB4);
+}
 
-uint8_t mcp2515_init(){
-    init_spi();
-    mcp2515_reset();
-}; 
+uint8_t mcp2515_init ()
+{
+	uint8_t value ;
+	init_spi() ; // Initialize SPI
+	mcp2515_reset () ; // Send reset - command
+	// Self - test
+	mcp2515_write( MCP_CANSTAT , & value );
+	if (( value & MODE_MASK ) != MODE_CONFIG ) {
+		printf (" MCP2515 is NOT in configuration mode after reset !\n");
+		return 1;
+	}
+	// More initialization
+	return 0;
+}
