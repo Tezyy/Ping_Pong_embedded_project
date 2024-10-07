0284d44cd32cf580b9c6c948b880345b9b1d5d2a
margot-frances <mgfrance@stud.ntnu.no>
2024-10-07 19:18:31 +0200

    Update MCP2515_driver.h

--- Node_1/MCP2515_driver.h
+++ Node_1/MCP2515_driver.h
@@ -4,17 +4,9 @@
 #define F_CPU 4915200UL
 #include <util/delay.h>
 
-#include <avr/io.h>
-#include <avr/sleep.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdint.h> 
-#include "spi.h"
-#include "MCP2515.h"
+uint8_t mcp2515_read(uint8_t adress);
 
-uint8_t read(uint8_t adress);
-
-void write(uint8_t adress, uint8_t data );
+uint8_t mcp2515_write(uint8_t adress, uint8_t data );
 
 void req_to_send(uint8_t stat);
 
@@ -24,6 +16,6 @@ void mcp2515_modify_bit(uint8_t address, uint8_t mask, uint8_t data);
 
 void mcp2515_reset();
 
-uint8_t mcp2515_init(); 
+uint8_t mcp2515_init();
 
 #endif
