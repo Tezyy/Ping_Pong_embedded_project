c83646ab526100e18fcba40f9adf8dd7fcdf7d22
Alix <128033856+Ali15130@users.noreply.github.com>
2024-10-14 11:55:53 +0200

    Update main.c

--- Node_2/main.c
+++ Node_2/main.c
@@ -7,12 +7,18 @@
 #include <stdlib.h>
 #include <stdint.h> 
 
+#include <sam.h>
+
+#include servo.h
 #include can.h
 #include time.h
 #include uart.h
 
 int main(void)
 {
+    	SystemInit(); // Initialize the system
+        configure_pwm_channel1(); // Configure PWM on channel 1
+
 	uart_init(84000000, 9600);
 	
 	while(1){
