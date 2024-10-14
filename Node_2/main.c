68255c3e753613f98d526fe1bf671b39aae0c6c8
Alix <128033856+Ali15130@users.noreply.github.com>
2024-10-14 11:24:55 +0200

    Update main.c

--- Node_2/main.c
+++ Node_2/main.c
@@ -1,3 +1,13 @@
+#define F_CPU 4915200UL
+#include <util/delay.h>
+
+#include <avr/io.h>
+#include <avr/sleep.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h> 
+
 #include can.h
 #include time.h
 #include uart.h
+
