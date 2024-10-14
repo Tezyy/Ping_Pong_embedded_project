7326f45b5bfd566bbbc3c036aaa5ebaf8481547d
Alix <128033856+Ali15130@users.noreply.github.com>
2024-10-14 16:24:37 +0200

    Update main.c

--- Node_2/main.c
+++ Node_2/main.c
@@ -1,28 +1,24 @@
-#define F_CPU 4915200UL
-#include <util/delay.h>
+#define F_CPU 84000000UL
 
-#include <avr/io.h>
-#include <avr/sleep.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdint.h> 
+#include <stdint.h>
 
 #include <sam.h>
 
-#include servo.h
-#include can.h
-#include time.h
-#include uart.h
+#include "uart.h"
+
 
 int main(void)
 {
 	SystemInit(); // Initialize the system
-	configure_pwm_channel1(); // Configure PWM on channel 1
+	//configure_pwm_channel1(); // Configure PWM on channel 1
 
 	uart_init(84000000, 9600);
 	
 	while(1){
+		printf("Hello");
 	}
-			
+	
 	return(0);
 }
