35dbd50e3515c50237f876f63990fe6bc8ddf2d1
Thibault Pouyanne <93395244+Tezyy@users.noreply.github.com>
2024-10-28 16:37:47 +0100

    update

---
+++ Node_2/Node_2/main.c
@@ -0,0 +1,18 @@
+/*
+ * Node_2.c
+ *
+ * Created: 28/10/2024 16:30:03
+ * Author : thiba
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
--- Node_2/PWM_driver.h
+++ Node_2/PWM_driver.h
@@ -6,4 +6,6 @@
 // Function to configure PWM on channel 1 (PB13)
 void configure_pwm_channel1(void);
 
+void PWM_set_duty_cycle(float duty_cycle);
+
 #endif // SERVO_H
