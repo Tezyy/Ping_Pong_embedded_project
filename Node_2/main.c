4d58ae1dc3219efab0ce26ed2d2d5ad07fcf0996
Alix <alix.boudou@gmail.com>
2024-10-28 14:26:20 +0100

    Node 2 CAN OK

--- Node_2/main.c
+++ Node_2/main.c
@@ -1,42 +1,44 @@
-#define F_CPU 84000000UL
-
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdint.h>
-
-#include <sam.h>
-
-#include "servo.h"
-#include "uart.h"
 #include "can.h"
+#include <sam.h>
+#include <stdarg.h>
+#include <stdio.h>
+#include <string.h>
 
-CanInit bit_timing = {.phase2 = 5, .propag = 1, .phase1 = 6, .sjw = 0, .brp = 41, .smp = 0};
+// Import UART from Node 2 starter code, then edit include path accordingly.
+// Also, remember to update the makefile
+#include "uart.h"
+#include "servo.h"
+
+#define F_CPU 84000000
+#define BAUDRATE 9600
+
+CanInit_t bit_timing = {.phase2 = 5, .propag = 1, .phase1 = 6, .sjw = 0, .brp = 41, .smp = 0};
 
 CanMsg receive_can;
 
-int main(void)
+
+int main()
 {
-	SystemInit(); // Initialize the system
-	//configure_pwm_channel1(); // Configure PWM on channel 1
+	SystemInit();
+	configure_pwm_channel1();
 
-	uart_init(84000000, 9600);
-	can_init(bit_timing, 0); // 0 for no RX interrupt/1 for RX interrupt
+	WDT->WDT_MR = WDT_MR_WDDIS; // Disable Watchdog Timer
 
-	CanMsg received_msg;
+	//PMC->PMC_PCER0 |= (1 << ID_PIOB);  // Enable clock for PIOB
+	//PIOB->PIO_OER = PI_PB13; //Configure PB13 as an output
 	
-	while(1){
-		//printf("Hello");
-		// Check if a CAN message has been received
-        	if (can_rx(&received_msg)) {
-           		// Print the received message
-           		can_printmsg(received_msg);
+	uart_init(F_CPU, BAUDRATE);
+	can_init(bit_timing, 0);
 
-		//Joystick x and y are stored in the first two bytes of the CAN message
-        		uint8_t joystick_x = received_msg.byte[0];
-       			uint8_t joystick_y = received_msg.byte[1];
-				printf("Received Joystick Coordinates - X: %d%%, Y: %d%%\n", joystick_x, joystick_y);
-		}	
+
+	while (1)
+	{
+		while (can_rx(&receive_can)){
+			printf("Receiving");
+			can_printmsg(receive_can);
+			}
+// 	PIOB->PIO_CODR = PIO_PB13;
+// 	PIOB->PIO_sODR = PIO_PB13;
+	
 	}
-	
-	return(0);
 }
