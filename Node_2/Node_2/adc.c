d25ca85f711c091d26de820f39ee783b8f9e5c75
Thibault Pouyanne <93395244+Tezyy@users.noreply.github.com>
2024-11-04 14:47:47 +0100

    change of adc_read

--- Node_2/Node_2/adc.c
+++ Node_2/Node_2/adc.c
@@ -6,12 +6,15 @@
 
 
 void adc_init(void){
-	
+
+	PMC->PMC_PCER1 |= 1 << (ID_ADC - 32); 	//set the clock : ID ADC = 37 mais registre séparé en 2 
+											//donc -32 pour arriver au bon endroit, 
+											//2 regsitres PMC là on est dans le 1, et on veut 5	
 	ADC->ADC_MR = ADC_MR_FREERUN; 	//Set ADC to free run mode
 
 	ADC->ADC_CHER = ADC_CHER_CH0; 	//Set AD0 en output
 	
-	PMC->PMC_PCER1 |= 1 << (ID_ADC - 32); 	//ID ADC = 37 mais registre séparé en 2 donc -32 pour arriver au bon endroit, 2 regsitres PMC là on est dans le 1, et on veut 5
+
 	
 	ADC->ADC_CR = ADC_CR_START; 	//Start ADC conversion
 	
@@ -19,7 +22,7 @@ void adc_init(void){
 
 
 
-void adc_read(){
-	uint32_t ir_level = ADC->ADC_CDR[0];
-	printf(ir_level);
+uint16_t adc_read(){
+	uint16_t ir_level = ADC->ADC_CDR[0];
+	return(ir_level);
 }
