3e503949da6fa0af7d9eb73029530d16ff6a5234
Alix <128033856+Ali15130@users.noreply.github.com>
2024-11-06 11:21:49 +0100

    Create timer_score.h

---
+++ Node_2/timer_score.h
@@ -0,0 +1,10 @@
+#ifndef TIMER_SCORE_H
+#define TIMER_SCORE_H
+
+#include <stdint.h>
+
+void start_score_timer(void);
+uint64_t stop_score_timer(void);
+void reset_score_timer(void);
+
+#endif // TIMER_SCORE_H
