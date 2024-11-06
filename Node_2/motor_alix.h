a7448603fd0020088f39575d63d8f04eac4a7679
Alix <128033856+Ali15130@users.noreply.github.com>
2024-11-06 13:47:25 +0100

    Update motor_alix.h

--- Node_2/motor_alix.h
+++ Node_2/motor_alix.h
@@ -1,17 +1,17 @@
-#ifndef MOTOR_H
-#define MOTOR_H
+// #ifndef MOTOR_H
+// #define MOTOR_H
 
-#include <stdint.h>
+// #include <stdint.h>
 
-// PWM configuration constants
-#define PWM_PERIOD 20000    // PWM period in microseconds (20 ms)
-#define PWM_MAX 19999       // Maximum pulse width
-#define PWM_MIN 0           // Minimum pulse width
-#define MID_PULSE_WIDTH 1500 // Middle pulse width, often around 1.5 ms for a servo at center position
+// // PWM configuration constants
+// #define PWM_PERIOD 20000    // PWM period in microseconds (20 ms)
+// #define PWM_MAX 19999       // Maximum pulse width
+// #define PWM_MIN 0           // Minimum pulse width
+// #define MID_PULSE_WIDTH 1500 // Middle pulse width, often around 1.5 ms for a servo at center position
 
-// Function prototypes
-void PWM_motor_init(void);
-void set_PWM_duty_motor(uint16_t pulse_width);
-uint16_t PWM_value_motor(int8_t input_joystick);
+// // Function prototypes
+// void PWM_motor_init(void);
+// void set_PWM_duty_motor(uint16_t pulse_width);
+// uint16_t PWM_value_motor(int8_t input_joystick);
 
-#endif // MOTOR_H
+// #endif // MOTOR_H
