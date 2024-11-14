77b86dc86fab838682beb3f445fa5dac29cfd065
margot-frances <mgfrance@stud.ntnu.no>
2024-11-14 12:05:59 +0100

    Update game.c

--- Node_2/game.c
+++ Node_2/game.c
@@ -9,9 +9,6 @@
 static int timer_now = 0;
 static int last_time = 0;
 
-
-
-
 //I assume value will between 0 and 2100 if the IR sensor has been covered or not
 //starting_time of the game
 Time score_counter (int value, int starting_time){
@@ -19,17 +16,13 @@ Time score_counter (int value, int starting_time){
     static ancient_tick = 0;
     
 	static Time t = (Time){.hours = 0, .minutes = 0, .seconds = 0, .msecs = 0};
-    //static uint64_t time_controller = 0;
-
     
-    if ( value < 500){ //trouver valeur
-	
+    if ( value < 1000){ //Reactivity of knowing when the IR sensor has been covered
 		
 		timer_now=time_now();
 		if (!(totalSeconds((uint64_t)(timer_now-last_time)) < 5) ){
 			printf("timer now :%d\r\n",(int)timer_now);
 			t=time_split((uint64_t)(timer_now-starting_time));
-			//printf("t: " time_fmtStr "\n", time_fmtParam(t));
 			last_time=timer_now;
 			return t;
 	}
@@ -44,30 +37,26 @@ void game_init(void){
 	//enable pin 53 as output
 	REG_PIOA_PER = PIO_PER_P11;
 	REG_PIOA_OER = PIO_PER_P11;
-	//set as high as it is active low
-	//PIOA-> PIO_SODR |= PIO_PA16;
-	
-
 }
-
 
 void solenoid (uint8_t state){
 	static state_before = 0;
 	 if (state_before != state) {
 		 if (state == 1) {
-			 // Mettre pin 53 à un niveau bas poru trigger
+			 // Mettre pin 53 Ã  un niveau bas poru trigger
 			 REG_PIOA_SODR = PIO_SODR_P11;
-			 printf("0000000000_____________00000000000000000000000000\r\n");
+			 printf("0000000000_____________00000000000000000000000000\r\n"); //for debugging
 			 } 
 			 else {
-			 // Mettre PB27 à un niveau bas
+			 // Mettre PB27 Ã  un niveau bas
 			 REG_PIOA_CODR = PIO_CODR_P11;
 			 printf("22222222222222222222222222222222222222222222\r\n");
 			 
 		 }
-		 
-		 // Mettre à jour l'état précédent
+		 // Mettre Ã  jour l'Ã©tat prÃ©cÃ©dent
 		 state_before = state;
 	 }
- }
-	
+ }
+
+	
+
