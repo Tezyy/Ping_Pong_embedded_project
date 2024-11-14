d22add77fbd74009b4a1c7b29244cb1b0e315026
margot-frances <mgfrance@stud.ntnu.no>
2024-11-14 12:06:34 +0100

    Update main.c

--- Node_2/main.c
+++ Node_2/main.c
@@ -7,7 +7,7 @@
 #include "uart.h"
 #include "motor.h"
 #include "encoder.h"
-
+#include "PI.h"
 
 #include <sam.h>
 #include <stdarg.h>
@@ -20,7 +20,6 @@
 #define ID_JOYSTICK 12
 #define ID_BUTTON_RIGHT 13
 
-
 CanInit_t bit_timing = {.phase2 = 5, .propag = 1, .phase1 = 6, .sjw = 0, .brp = 41, .smp = 0};
 
 CanMsg receive_can;
@@ -29,7 +28,6 @@ uint16_t pwm_duty_joystick ;
 uint32_t adc_value ;
 static uint64_t score = 0;
 Time SCORE_final =(Time){.hours = 0, .minutes = 0, .seconds = 0, .msecs = 0};
-//extern int score = 0;
 static uint8_t button_state = 0;
 static uint8_t game_start = 0; //0 if the game is not started, 1 if it is
 static uint8_t game_running =0;
@@ -41,9 +39,6 @@ int main()
 	PWM_init();   // Initialize PWM
 
 	WDT->WDT_MR = WDT_MR_WDDIS; // Disable Watchdog Timer
-
-	//PMC->PMC_PCER0 |= (1 << ID_PIOB);  // Enable clock for PIOB
-	//PIOB->PIO_OER = PI_PB13; //Configure PB13 as an output
 	
 	uart_init(F_CPU, BAUDRATE);
 	can_init(bit_timing, 0);
@@ -53,7 +48,6 @@ int main()
 	init_TC2_quadrature_mode();
 	PWM_motor_init();
 	game_init();
-	//score = time_now();
 	while (1)
 	{
 		if ((game_running !=1) && (game_end !=1)){
@@ -63,44 +57,44 @@ int main()
 		set_PWM_duty(0);
 		}
 		if(can_rx(&receive_can)){ // && game_running !=1
-			//printf("message id %d\r\n", receive_can.id);
 			if (receive_can.id == 11) {
 				
+				//all initialization for the game to start properly
 				reset_score_timer();
 				start_score_timer();
 				printf("game running...");
 				game_start=1;
 				game_running=1;
-				
-				//all initialization for the game to start properly
 			}
 		}
-		//printf("game running %u\r\n", game_running);
 		
 		if (game_running !=0){
 			can_rx(&receive_can);
 			
 			switch (receive_can.id){
 				case ID_JOYSTICK :
-					//printf("id joystick");
 					set_PWM_duty(PWM_value((receive_can.byte[1]-128))); //servo
 					
+					int16_t encoder_position = read_encoder_position();
+					
 					//Joystick moteur
-					if (receive_can.byte[0]>180){
-						PIOC->PIO_SODR = PIO_PC23;  // Forward direction (PHASE = 1)
-						set_PWM_duty_motor(7000);
+					int16_t u = PI(receive_can.byte[0],encoder_position);
+					u = u*5;
+					printf("u=%d\n\r", u);
+					if (u>0){
+						PIOC->PIO_CODR = PIO_PC23;
+						set_PWM_duty_motor(u);
 					}
-					else if (receive_can.byte[0]<80){
-						PIOC->PIO_CODR = PIO_PC23;  // Reverse direction (PHASE = 0)
-						set_PWM_duty_motor(7000);
-					}
-					else if (receive_can.byte[0]>100 && receive_can.byte[0]<160){
-						set_PWM_duty_motor(0);
+					else if (u<0){
+						PIOC->PIO_SODR = PIO_PC23;
+						u = abs(u);
+						set_PWM_duty_motor(u);
 					}
 
 					solenoid (0);
 					
 					break;
+					
 				case ID_BUTTON_RIGHT :
 					button_state = receive_can.byte[0];
 					printf("test button state : %u\r\n",button_state);
@@ -115,22 +109,8 @@ int main()
 				score =stop_score_timer();
 				printf("SCORE %f seconds \r\n",totalSeconds(score));
 				game_running=0;
-				game_end =0;
-				
-			}
-			
-		}
-// 		if (game_end !=0){
-// 			printf("TU AS PERDU...");
-// 		}
-		
-		uint16_t position =read_encoder_position();
-		//printf ("encoder position : %u \r\n", position);
-		//for (volatile int i =0; i<1000000; i++);
-// 		printf("adc value adr0 : %d\r\n", score);
-		
-		
-		
+				game_end =0;			
+			}		
+		}	
 	}
-	
 }
