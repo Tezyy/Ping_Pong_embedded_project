1f44bdc73f3854a0b7667be711de9d9e655630ca
margot-frances <mgfrance@stud.ntnu.no>
2024-11-14 12:06:16 +0100

    Update game.h

--- Node_2/game.h
+++ Node_2/game.h
@@ -6,10 +6,8 @@
 #include "time.h"
 #include "uart.h"
 
-//I assume value will between 0 and 100 if the IR sensor has been covered or not
 Time score_counter (int value, int starting_time);
 void game_init (void);
 void solenoid (uint8_t state);
-
 
 #endif
