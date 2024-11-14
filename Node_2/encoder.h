c83519b68abfc1496f76938acd3f38f7c925da31
margot-frances <mgfrance@stud.ntnu.no>
2024-11-14 12:05:12 +0100

    Update encoder.h

--- Node_2/encoder.h
+++ Node_2/encoder.h
@@ -1,12 +1,9 @@
 #ifndef ENCODEUR_H
 #define ENCODEUR_H
 
-#include <sam.h> // Include SAM-specific header for register definitions
+#include <sam.h>
 
-// Initialize TC2 in quadrature mode to decode encoder signals
 void init_TC2_quadrature_mode(void);
-
-// Read the current encoder position from TC2
 uint8_t read_pin_PC25();
 uint8_t read_pin_PC26();
 uint16_t read_encoder_position(void);
