10529143f6d61a5f602e9c9a4e092af417cc6bba
margot-frances <mgfrance@stud.ntnu.no>
2024-11-14 12:07:06 +0100

    Update motor.h

--- Node_2/motor.h
+++ Node_2/motor.h
@@ -3,15 +3,12 @@
 
 #include <stdint.h>
 
-// PWM configuration constants
-#define PWM_PERIOD 20000    // PWM period in microseconds (20 ms)
-#define PWM_MAX 19000      // Maximum pulse width
-#define PWM_MIN 0           // Minimum pulse width
-#define MID_PULSE_WIDTH 1500 // Middle pulse width, often around 1.5 ms for a servo at center position
+#define PWM_PERIOD 20000   
+#define PWM_MAX 19000      
+#define PWM_MIN 0          
+#define MID_PULSE_WIDTH 1500 
 
-// Function prototypes
 void PWM_motor_init(void);
 void set_PWM_duty_motor(uint16_t pulse_width);
-//uint16_t PWM_value_motor(int8_t input_joystick);
 
 #endif // MOTOR_H
